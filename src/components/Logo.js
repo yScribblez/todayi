8ca3539e8f07fa0145d45ef7138f681239cd63d7
yScribblez <yscribblezz@gmail.com>
2020-07-16 01:56:41 -0500

    minor formatting update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { Grid, Icon, Image } from "semantic-ui-react";
+import React from 'react';
+import { Image } from 'semantic-ui-react';
 
-import logo from "../assets/logo.png";
+import logo from '../assets/logo.png';
 
 const Logo = () => {
   return (
-    <div><Image inline src={logo} size="mini" style={{margin: '0em 1em 0em 0em'}} /> T O D A Y I</div>
+    <div>
+      <Image
+        inline
+        src={logo}
+        size="mini"
+        style={{ margin: '0em 1em 0em 0em' }}
+      />{' '}
+      T O D A Y I
+    </div>
   );
 };
-// <div><Icon name="pencil alternate" /> T O D A Y I</div>
 
 export default Logo;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Grid } from 'semantic-ui-react';
 
 import Dashboard from './views/Dashboard';
 
-ReactDOM.render(
-  <Dashboard />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
